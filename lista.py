20a76c63cc3f22cf30b4a46ff6e4048d7fc378bf
Mayara Cunha <mayara.c.cunha@gmail.com>
2025-12-04 19:42:53 -0300

    Adição de novos exercícios

---
+++ lista.py
@@ -0,0 +1,15 @@
+produto: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "videogame"
+
+produtos: list = []
+
+produtos.append(produto)
+produtos.append(produto_2)
+produtos.append(produto_3)
+
+numeros = []
+numeros.extend(range(0,5))
+
+print(numeros)
+print(produtos)
